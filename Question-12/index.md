c7e1f041d20f43b66f6b145387bb8e5ad9bd4ab8
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-05-21 00:25:15 +0530

    coercion

---
+++ Question-12/index.md
@@ -0,0 +1,8 @@
+# What is type coercion in JS?
+
+--> Type coercion is the automatic conversion 0f values from one data type to another during certain operations or comparisons.
+
+# uses of coercion
+
+1. Type coercion can be used during String and Number concatenation.
+2. Type coercion can be used whil using Comparison operators.
---
+++ Question-12/script.js
@@ -0,0 +1,16 @@
+let str = "7"
+let number = 7;
+let bool = true;
+let nullValue = null;
+
+// type coercion - automatic conversion
+
+// console.log(str + number); // output: valuebound
+// console.log(number + bool); // output: 8
+
+
+// comparisons
+
+console.log(number == str);     // false
+console.log(bool == 1);         // true
+console.log(bool + nullValue);  // 1
