daa28ab574c6c4f5c489c55b0c67e59fa3706c88
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-05-21 00:23:36 +0530

    typeof

---
+++ Question-11/index.md
@@ -0,0 +1,5 @@
+# What is the use of typeof operator?
+
+--> typeof operator is used to determine the type of each variable.
+--> Real application use -> typeOf operator can be used to
+    validate the data received from external
---
+++ Question-11/script.js
@@ -0,0 +1,22 @@
+let num = 7;
+let str = "valuebound";
+let bool = true;
+let obj = {
+    key: "value"
+};
+let arr = [1, 2, 3, 4];
+let func =  function(){
+    console.log("hello");
+}
+
+
+// using typeof
+
+console.log(typeof (num));
+console.log(typeof (str));
+console.log(typeof (bool));
+console.log(typeof (obj));
+console.log(typeof (arr));
+console.log(typeof (func));
+
+
